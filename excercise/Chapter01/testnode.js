b4c7dcd2fb4d883dd6f3df0dc31bacce516e6c05
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2024-07-29 22:07:12 +0330

    first day

---
+++ excercise/Chapter01/testnode.js
@@ -0,0 +1,13 @@
+var a = 12;
+if (true) {
+  var b = 56;
+  let c = 89;
+  console.log("In the braces:");
+  console.log("a = " + a);
+  console.log("b = " + b);
+  console.log("c = " + c);
+}
+console.log("After the brace:");
+console.log("a = " + a);
+console.log("b = " + b);
+console.log("c = " + c);
